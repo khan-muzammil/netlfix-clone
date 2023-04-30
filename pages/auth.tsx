86913a2667563cb145ec75880de294c6fcbc79f4
Muzammil Khan <muzammil.khan@outlook.in>
2023-04-30 19:37:02 +0530

    Removed Github auth

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -2,7 +2,6 @@ import Input from "@/components/Input";
 import axios from "axios";
 import { signIn } from "next-auth/react";
 import { useCallback, useState } from "react";
-import { FaGithub } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
 
 export default function Auth() {
@@ -91,12 +90,6 @@ export default function Auth() {
                 className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
               >
                 <FcGoogle size={32} />
-              </div>
-              <div
-                onClick={() => signIn("github", { callbackUrl: "/profiles" })}
-                className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
-              >
-                <FaGithub size={32} />
               </div>
             </div>
             <p className="text-neutral-500 mt-12">
